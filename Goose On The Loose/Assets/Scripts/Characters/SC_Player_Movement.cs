d1edde143742d6de4dcf9b8a6cd2488c1fa28464
Jibrill Murphy <78186997+JibDib@users.noreply.github.com>
2023-08-08 14:29:19 +1000

    Updated player controller
    Added animation support, created placeholder animations and animation controller - Jibrill

--- Goose On The Loose/Assets/Scripts/Characters/SC_Player_Movement.cs
+++ Goose On The Loose/Assets/Scripts/Characters/SC_Player_Movement.cs
@@ -6,20 +6,46 @@ public class SC_Player_Movement: MonoBehaviour
 {
 
     public float moveSpeed = 5f;
-
     public Rigidbody2D characterRigidBody;
-
+    public Animator characterAnimationController;
     private Vector2 inputData;
 
     void Update()
+    {
+        GetPlayerInput();
+        UpdatePlayerAnimator();
+   
+    }
+
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+
+
+    public void GetPlayerInput()
     {
         inputData.x = Input.GetAxisRaw("Horizontal");
         inputData.y = Input.GetAxisRaw("Vertical");
     }
 
-    void FixedUpdate()
+    public void UpdatePlayerAnimator()  // Updates variables in player animator
     {
-        characterRigidBody.MovePosition(characterRigidBody.position + inputData * moveSpeed * Time.fixedDeltaTime);
+        characterAnimationController.SetFloat("Horizontal_Input", inputData.x);
+        characterAnimationController.SetFloat("Vertical_Input", inputData.y);
+        characterAnimationController.SetFloat("Speed", inputData.sqrMagnitude);
+
+        if (inputData.sqrMagnitude != 0)
+        {
+            characterAnimationController.SetFloat("Idle_Direction_Horizontal", inputData.x);
+            characterAnimationController.SetFloat("Idle_Direction_Vertical", inputData.y);
+        }
+    }
+
+    public void MovePlayer()
+    {
+        characterRigidBody.MovePosition(characterRigidBody.position + inputData.normalized * moveSpeed * Time.fixedDeltaTime);
     }
 
 }
