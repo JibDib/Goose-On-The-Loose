769c371c9b0a9cb31e021be07bb83b9b38c467aa
Jibrill Murphy <78186997+JibDib@users.noreply.github.com>
2023-08-29 20:30:54 +1000

    Fixed up player honk script
    You can now honk in the direction you click the mouse

--- Goose On The Loose/Assets/Scripts/Characters/SC_Player_Honk.cs
+++ Goose On The Loose/Assets/Scripts/Characters/SC_Player_Honk.cs
@@ -5,30 +5,52 @@ using UnityEngine;
 public class SC_Player_Honk : MonoBehaviour
 {
 
+
+    public Camera playerCam;
     private SC_Audio_Manager audioManager;
+    public GameObject honkPrefab;
+    public Rigidbody2D playerRigidbody;
+    public GameObject gooseHead;
+    private Rigidbody2D gooseHeadRigidBody;
+    private Transform gooseHeadTransform;
+    public float honkForce = 20f;
+
+
     private string Honk;
-    private GameObject self;
-    public GameObject honkParticle;
+    private Vector2 mousePosition;
+    private Vector2 lookDirection;
 
 
     private void Awake()
     {
         audioManager = GetComponent<SC_Audio_Manager>();
-        Debug.Log(audioManager);
         Honk = "Honk";
-        self = this.gameObject;
+        gooseHeadRigidBody = gooseHead.GetComponent<Rigidbody2D>();
+        gooseHeadTransform = gooseHead.GetComponent<Transform>();
     }
 
     private void Update()
     {
+
+        mousePosition = playerCam.ScreenToWorldPoint(Input.mousePosition);
+        lookDirection = mousePosition - playerRigidbody.position;
+        float lookAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+        gooseHeadRigidBody.rotation = lookAngle;
+
+
         if (Input.GetMouseButtonDown(0))
         {
             audioManager.PlayRandomSound(Honk);
-            Debug.Log(Honk);
-            Instantiate(honkParticle, self.transform.position, Quaternion.identity);
+            FireHonk();
         }
+
     }
 
+    public void FireHonk()
+    {
+        GameObject honk = Instantiate(honkPrefab, playerRigidbody.position, gooseHeadTransform.rotation);
+        honk.GetComponent<Rigidbody2D>().AddForce(gooseHeadTransform.up * honkForce, ForceMode2D.Impulse);
+    }
     
 
 }
